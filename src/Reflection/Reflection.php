142bd3525ca7496dcc196085b3ab87fe2088ab26
MartkCz <martk494@gmail.com>
2022-02-13 12:29:57 +0100

    add Reflection::getAttributeObject

---
+++ src/Reflection/Reflection.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace Utilitte\Php\Reflection;
+
+use ReflectionClass;
+use ReflectionMethod;
+
+final class Reflection
+{
+
+	/**
+	 * @template T
+	 * @param class-string<T> $name
+	 * @return T|null
+	 */
+	public static function getAttributeObject(ReflectionMethod|ReflectionClass $reflection, string $name): ?object
+	{
+		$attribute = $reflection->getAttributes($name)[0] ?? null;
+
+		if (!$attribute) {
+			return null;
+		}
+
+		return $attribute->newInstance();
+	}
+
+}
