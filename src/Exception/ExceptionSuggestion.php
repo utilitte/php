736e9e6f4d992555842dd231451b6ea2ee6b5452
MartkCz <martk494@gmail.com>
2022-07-20 11:49:50 +0200

    ExceptionSuggestion add type int

--- src/Exception/ExceptionSuggestion.php
+++ src/Exception/ExceptionSuggestion.php
@@ -10,8 +10,12 @@ final class ExceptionSuggestion
 	/**
 	 * @param mixed[] $possibilities
 	 */
-	public static function didYouMean(array $possibilities, string $value): string
+	public static function didYouMean(array $possibilities, string|int $value): string
 	{
+		if (is_int($value)) {
+			return '.';
+		}
+
 		$suggestion = Helpers::getSuggestion($possibilities, $value);
 
 		if (!$suggestion) {
